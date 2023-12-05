a806ee523854f385b0d394a697bf3dc29571c872
chris ho <98445729+ho0527@users.noreply.github.com>
2023-12-05 19:45:30 +0800

    new: 20231204
    - 51nMD建檔

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,8 @@ urlpatterns=[
     # 技能競賽
     path("45regional/",include("45regional.url"),name="45regional"), # 45regional
     path("46nationalmoduled/",include("46nationalmoduled.url"),name="46nationalmoduled"), # 46nationalmoduled
+    path("51nationalmoduled/",include("51nationalmoduled.url"),name="51nationalmoduled"), # 51nationalmoduled
     path("51regional/",include("51regional.url"),name="51regional"), # 51regional
-    path("52nationalmodulee/",include("52nationalmodulee.url"),name="52nationalmodulee"), # 46nationalmoduled
     path("53regional/",include("53regional.url"),name="53regional"), # 53regional
     path("worldskill2022modulec/",include("ws2022modulec.url"),name="ws2022modulec"), # worldskill2022modulec
 
