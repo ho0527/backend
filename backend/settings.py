0ee2b47980805c360a0c82d4e831b212e39f1fc3
chris960527 <98445729+ho0527@users.noreply.github.com>
2024-06-30 18:09:10 +0800

    new: 20240630

--- backend/settings.py
+++ backend/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS=[
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "drf_yasg"
 ]
 
@@ -24,6 +25,7 @@ MIDDLEWARE=[
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+	"corsheaders.middleware.CorsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -97,3 +99,5 @@ SWAGGER_SETTINGS={
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"upload")
+
+CORS_URLS_REGEX = r"^.*$"
