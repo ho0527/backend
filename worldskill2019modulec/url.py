58a21655e4e52ef8f864e47ad9c905640dff2db9
chris960527 <98445729+ho0527@users.noreply.github.com>
2025-04-22 21:58:04 +0800

    fix: 修正報錯
    updatedate->20250422
    
    worldskill2019modulec

--- worldskill2019modulec/url.py
+++ worldskill2019modulec/url.py
@@ -7,8 +7,8 @@ from . import user
 
 urlpatterns=[
     re_path("^api/v1/event$",event.geteventlist,name="geteventlist"),
-    re_path("^api/v1/organizers/(?P<organizer-slug>.+)/event/(?P<event-slug>.+)$",event.getevent,name="getevent"),
-    re_path("^api/v1/organizers/(?P<organizer-slug>.+)/event/(?P<event-slug>.+)/registration$",event.newevent,name="newevent"),
+    re_path("^api/v1/organizers/(?P<organizer_slug>.+)/event/(?P<event_slug>.+)$",event.getevent,name="getevent"),
+    re_path("^api/v1/organizers/(?P<organizer_slug>.+)/event/(?P<event_slug>.+)/registration$",event.newevent,name="newevent"),
     re_path("^api/v1/organizers/registration$",event.getuserevent,name="getuserevent"),
 
     re_path("^api/v1/login$",user.login,name="login"),
